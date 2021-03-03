682ee9d496d09b42910910daff9ea29f24e1f2e2
sweetAme <reskeleton@gmail.com>
2021-03-03 12:38:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length > 0) { 
+    let minValue = Number.MAX_VALUE;
+    for (let i of array) {
+      if (i < minValue) {
+        minValue = i;
+      }
+    }
+    return minValue;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length > 0) {
+    let maxValue = Number.MIN_VALUE;
+    for (let i of array) {
+      if (i > maxValue) {
+        maxValue = i;
+      }
+    }
+    return maxValue;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length > 0) {
+    let sum = 0;
+    for (let i of array) {
+      sum += i;
+    }
+    average = (sum / array.length);
+    return average;
+  } else {
+    return 0;
+  }
 }
